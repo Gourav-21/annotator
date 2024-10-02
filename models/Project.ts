466ccc0c36b93f1cc56d901f9a6ebc04d432225b
Gourav-21 <rahul.dora21@gmail.com>
2024-10-02 14:49:30 +0530

    removed comments from models

--- models/Project.ts
+++ models/Project.ts
@@ -17,15 +17,8 @@ export const Project = models?.Project || model('Project', projectSchema);
 const templateSchema = new Schema({
   name: { type: String, required: true },
   created_at: { type: Date, default: Date.now },
-  // type: { type: String, enum: ['text', 'audio', 'video', 'image'], required: true },
   content: { type: String, required: true },
-  // question: { type: Object, required: true },
-  // media_uri: { type: String, required: false },
   project: { type: Schema.Types.ObjectId, ref: 'Project', required: true },
-  // Tasks: [{ type: Schema.Types.ObjectId, ref: 'Task', }],
-  // answer_type: { type: String, enum: ['text_field', 'checkbox_single', 'checkbox_multiple'], required: true },
-  // answer: { type: Object, required: false },
-  // status: { type: String, enum: ['pending', 'accepted', 'rejected', 'reassigned'], default: 'pending' },
 });
 
 export const Template = models?.Template || model('Template', templateSchema);
