f1665691ea1745b071706b68d29c5ba5395318af
Gourav-21 <rahul.dora21@gmail.com>
2024-10-06 16:57:06 +0530

    added sidebar

---
+++ lib/menu-list.ts
@@ -0,0 +1,101 @@
+import {
+  Tag,
+  Users,
+  Settings,
+  Bookmark,
+  SquarePen,
+  LayoutGrid,
+  LucideIcon
+} from "lucide-react";
+import { usePathname } from "next/navigation";
+
+type Submenu = {
+  href: string;
+  label: string;
+  active?: boolean;
+};
+
+type Menu = {
+  href: string;
+  label: string;
+  active: boolean;
+  icon: LucideIcon;
+  submenus?: Submenu[];
+};
+
+type Group = {
+  groupLabel: string;
+  menus: Menu[];
+};
+
+export function getMenuList(pathname: string): Group[] {
+  const pathName = usePathname();
+  const projectId = pathName.split("/")[pathName.split("/").length - 1]
+
+  if(projectId == "" || projectId == 'dashboard') {
+    return [
+      {
+        groupLabel: "",
+        menus: [
+          {
+            href: "/dashboard",
+            label: "Dashboard",
+            active: pathname.includes("/dashboard"),
+            icon: LayoutGrid,
+            submenus: []
+          }
+        ]
+      },
+      {
+        groupLabel: "Contents",
+        menus: [
+          {
+            href: "/",
+            label: "Projects",
+            active: pathname == '/',
+            icon: SquarePen,
+          },
+        ]
+      },
+    ];
+  }
+  
+
+  return [
+    {
+      groupLabel: "",
+      menus: [
+        {
+          href: "/dashboard",
+          label: "Dashboard",
+          active: pathname.includes("/dashboard"),
+          icon: LayoutGrid,
+          submenus: []
+        }
+      ]
+    },
+    {
+      groupLabel: "Contents",
+      menus: [
+        {
+          href: "/",
+          label: "Projects",
+          active: pathname == '/',
+          icon: SquarePen,
+        },
+        {
+          href: `/projects/${projectId}`,
+          label: "Templates",
+          active: pathname.includes("/projects") && !pathname.includes("/task"),
+          icon: Bookmark
+        },
+        {
+          href: `/projects/task/${projectId}`,
+          label: "Tasks",
+          active: pathname.includes("/task"),
+          icon: Tag
+        }
+      ]
+    },
+  ];
+}
