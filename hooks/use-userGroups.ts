9f027da9613247b013ee7d7ece91135731da60a9
Gourav-21 <rahul.dora21@gmail.com>
2024-10-23 14:40:01 +0530

    updated the chat hooks

--- hooks/use-userGroups.ts
+++ hooks/use-userGroups.ts
@@ -6,23 +6,25 @@ type Status = {
   userGroups: UserGroups[]
   setUserGroups: (data: UserGroups[]) => void
   removeUserGroup: (id: string) => void;
-  updateLastReadMessage: (data: Message) => void;
   getLastReadMessage: (id: string) => string | undefined;
+  updateLastReadMessage: (data: Message) => void;
   updateLastMessage: (data: Message) => void
 }
 
 const useUserGroups = create<Status>()((set, get) => ({
   userGroups: [],
   setUserGroups: (data: UserGroups[]) => set(() => ({ userGroups: data })),
-  removeUserGroup: (id: string) => set((state) => ({ userGroups: state.userGroups.filter((group) => group._id !== id) })),
-  updateLastReadMessage: (data: Message) => set((state) => ({ userGroups: state.userGroups.map((group) => group._id === data._id ? { ...group, lastReadMessage: data } : group) })),
+  removeUserGroup: (id: string) => set((state) => ({ userGroups: state.userGroups.filter((group) => group.group._id !== id) })),
+  updateLastReadMessage: (data: Message) => set((state) => ({ userGroups: state.userGroups.map((group) => group.group._id === data.group ? { ...group, lastReadMessage: data } : group) })),
   getLastReadMessage: (id: string) => {
     const { userGroups } = get();
-    return userGroups.find((group) => group.group._id === id)?.lastReadMessage?._id
+    // console.log(userGroups.find((group) => group._id === id)?.lastReadMessage?._id)
+    return userGroups.find((group) => group._id === id)?.lastReadMessage?._id
   },
   updateLastMessage: (data: Message) => set((state) => ({
-    userGroups: state.userGroups.map((group) => group.group._id === data.group ? { ...group, group: { ...group.group, lastMessage: data } } : group)
-  }))
+    userGroups: state.userGroups.map((group) =>
+      group.group._id === data.group ? { ...group, group: { ...group.group, lastMessage: data } } : group
+    )}))
 }))
 
 export default useUserGroups
