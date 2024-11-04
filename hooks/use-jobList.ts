591ec5b851beb8548bd29057480d4d32afb54275
Gourav-21 <rahul.dora21@gmail.com>
2024-11-04 19:01:08 +0530

    updated the joblist store

--- hooks/use-jobList.ts
+++ hooks/use-jobList.ts
@@ -11,18 +11,24 @@ interface Job {
 
 type Status = {
   Jobs: Job[]
+  setJobs: (data: Job[]) => void
   getJobs: () => Job[]
   setJob: (data: Job) => void
   removeJob: (id: string) => void
   getcompletedJobCount: () => number
+  getUncompletedJobCount: () => number
+  deleteCompleted: () => void
 }
 
 const useJobList = create<Status>()((set, get) => ({
   Jobs: [],
+  setJobs: (data: Job[]) => set({ Jobs: data }),
   getJobs: () => get().Jobs,
   setJob: (data: Job) => set({ Jobs: [...get().Jobs, data] }),
   removeJob: (id: string) => set({ Jobs: get().Jobs.filter((Job) => Job._id !== id) }),
   getcompletedJobCount: () => get().Jobs.filter((Job) => Job.completed == true).length,
+  getUncompletedJobCount: () => get().Jobs.filter((Job) => Job.completed == false).length,
+  deleteCompleted: () => set({ Jobs: get().Jobs.filter((Job) => Job.completed == false) }),
 }))
 
 export default useJobList
